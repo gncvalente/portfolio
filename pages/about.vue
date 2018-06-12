9ed4db72e5dd99d82bfc2fd48cc2a52407c88bc2
giancarlo valente <giancarlo.valente@gmail.com>
2018-06-12 04:29:47 +0200

    test static site

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<!-- pages/about.vue -->
+
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is Giancarlo Valente, I’m a self taught software developer based in South of Italy. I build modern applications for the web! I'm a technical writer, I write technical articles and tutorials for various platforms including <a href="https://scotch.io/@mezie">Scotch.io</a>.
+      </p>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<!-- pages/index.vue -->
+
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Giancarlo Valente</h1>
+      <h2 class="subtitle">a world class Software Developer based in South if Italy.</h2>
+    </div>
+  </section>
+</template>
